551965236aa80a2f181be966e957c2f485a65511
choyai <choyaichaiyo@gmail.com>
2017-10-17 06:14:14 +0700

    activities urls linked

--- FIBO_Portfolio/activities/urls.py
+++ FIBO_Portfolio/activities/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.activities_home, name='activities_home'),
+    url(r'^create', views.create, name='create'),
 ]
