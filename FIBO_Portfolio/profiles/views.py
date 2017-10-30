f44f50903e8e6d1949bcdda6c2cf6aa35d015969
aob787 <aob787@gmail.com>
2017-10-30 13:06:12 +0700

    update urls and views files.

--- FIBO_Portfolio/profiles/views.py
+++ FIBO_Portfolio/profiles/views.py
@@ -6,27 +6,45 @@ from django.template import loader
 def home(request):
     return render(request, 'profiles/index.html')
 
-def profile(request, user_id):
+def login(request):
+    return render(request, 'profiles/login.html')
+
+def forgotpassword(request, user_id):
     return render(request, 'profiles/profilepage.html')
 
-def edit(request, user_id):
-    return HttpResponse('This will be the profile edit page')
+def changepassword(request, user_id):
+    return render(request, 'profiles/changepassword.html')
+
+
+def profile(request, user_id):
+    return render(request, 'profiles/profilepage.html')
+def privacy(request, user_id):
+    return render(request, 'profiles/privacy.html')
+
+def personaledit(request, user_id):
+    return render('profile/personaledit.html')
+def academicedit(request, user_id):
+    return render(request, 'profiles/academicedit.html')
+def awardedit(request, user_id):
+    return render(request, 'profiles/awardedit.html')
+def workedit(request, user_id):
+    return render(request, 'profiles/worknexperienceedit.html')
+
+
 
 def academic(request, user_id):
     return render(request, 'profiles/academic.html')
 def award(request, user_id):
     return render(request, 'profiles/award.html')
-def changepassword(request, user_id):
-    return render(request, 'profiles/changepassword.html')
-def privacy(request, user_id):
-    return render(request, 'profiles/privacy.html')
-def profilepage(request, user_id):
-    return render(request, 'profiles/profilepage.html')
 def work(request, user_id):
     return render(request, 'profiles/work.html')
+
+def profilepage(request, user_id):
+    return render(request, 'profiles/profilepage.html')
+
+
 def organization(request):
     return render(request, 'profiles/organization.html')
 
 def aboutus(request):
     return render(request, 'profiles/aboutus.html')
-
