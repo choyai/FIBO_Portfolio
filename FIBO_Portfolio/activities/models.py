93891faeb914bc284e2f6d6cf4b8c9282465c357
Chaiyo <choyaichaiyo@gmail.com>
2017-10-30 16:42:46 +0700

    Moved the activity model back

--- FIBO_Portfolio/activities/models.py
+++ FIBO_Portfolio/activities/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 import profiles
+
+class Activity(models.Model):
+    name = models.CharField(max_length=250)
+    category = models.CharField(max_length=250, blank=True, null=True)
+    description = models.CharField(max_length=5000, blank=True, null=True)
+    location = models.CharField(max_length=250, blank=True, null=True)
+    startDate = models.DateField(blank=True, null=True)
+    endDate = models.DateField(blank=True, null=True)
+    isVerified = models.BooleanField(default = False)
--- FIBO_Portfolio/profiles/models.py
+++ FIBO_Portfolio/profiles/models.py
@@ -20,12 +20,4 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
-
-class Activity(models.Model):
-    name = models.CharField(max_length=250)
-    category = models.CharField(max_length=250, blank=True, null=True)
-    description = models.CharField(max_length=5000, blank=True, null=True)
-    location = models.CharField(max_length=250, blank=True, null=True)
-    startDate = models.DateField(blank=True, null=True)
-    endDate = models.DateField(blank=True, null=True)
-    isVerified = models.BooleanField(default = False)
+        
