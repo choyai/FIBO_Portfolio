4364a163bd67cc2e7b875ded06abe0c08b1c4f96
Chaiyo <choyaichaiyo@gmail.com>
2017-10-31 12:31:28 +0700

    Activities are now back in profiles app

--- FIBO_Portfolio/profiles/models.py
+++ FIBO_Portfolio/profiles/models.py
@@ -16,6 +16,9 @@ class Profile(models.Model):
     emailConfirmed = models.BooleanField(default=False)
     position = models.CharField(max_length = 100)
 
+    def get_absolute_url(self):
+        return reverse('activities:detail', kwargs={'pk': self.pk})
+
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
@@ -47,3 +50,44 @@ def user_dir_path(instance, filename):
 class UserImage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.FileField(upload_to=user_dir_path)
+
+class Activity(models.Model):
+    participants = models.ManyToManyField(
+        User,
+        through='Participation',
+        related_name='participant',
+        )
+    supervisors = models.ManyToManyField(
+        User,
+        through='Supervision',
+        related_name='supervisor',
+    )
+
+    name = models.CharField(max_length=250)
+    category = models.CharField(max_length=250, blank=True, null=True)
+    description = models.CharField(max_length=5000, blank=True, null=True)
+    location = models.CharField(max_length=250, blank=True, null=True)
+    startDate = models.DateField(blank=True, null=True)
+    endDate = models.DateField(blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('activities:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+class Supervision(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    supervisor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Participation(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    participant = models.ForeignKey(User, on_delete=models.CASCADE)
+    ifVerified = models.BooleanField(default = False)
+
+def activity_dir_path(instance, filename):
+    return 'activity_{0}/{1}'.format(instance, filename)
+
+class ActivityImage(models.Model):
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = activity_dir_path)
