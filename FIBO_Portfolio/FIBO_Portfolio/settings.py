06a6bb8863c457ecd04a40d06b75a678f387c628
Chaiyo <choyaichaiyo@gmail.com>
2017-11-14 14:40:06 +0700

    ignore migrations

--- FIBO_Portfolio/FIBO_Portfolio/settings.py
+++ FIBO_Portfolio/FIBO_Portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_view_permission',
     'profiles.apps.ProfilesConfig',
     'activities.apps.ActivitiesConfig',
     'django.contrib.admin',
