8fa5b380070323417ae01f8587520f6729e2c0c6
choyai <choyaichaiyo@gmail.com>
2017-11-24 12:02:06 +0700

    Site admin models

--- FIBO_Portfolio/profiles/admin.py
+++ FIBO_Portfolio/profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Ability)
+admin.site.register(Grade)
+admin.site.register(EducationBackground)
+admin.site.register(UserImage)
+admin.site.register(Activity)
+admin.site.register(ActivityImage)
